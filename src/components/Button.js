c4332d25c24a7b696993b8d64df3cd517350c15e
Lika <lika@Likas-MacBook-Air.local>
2023-05-31 04:32:40 +0400

    converted to functional component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,7 @@
-import React from 'react';
-
-export default class Button extends React.Component {
-    render() {
-        return (
-            <button 
-                onClick={this.props.handleClick}
-            >
-                {this.props.text}
-            </button>
-        );
-    }
+function Button({handleClick, text}) {
+    return (
+        <button onClick={handleClick}>
+            {text}
+        </button>
+    );
 }
